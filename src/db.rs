50ae76d3169b333a4c02f3f0efc33ea8df23d8eb
snakexgc <tj2577266613@gmail.com>
2026-02-15 19:37:51 +0800

    测试自动部署

--- src/db.rs
+++ src/db.rs
@@ -3,5 +3,5 @@ use std::sync::Arc;
 use worker::{D1Database, Env};
 
 pub fn get_db(env: &Arc<Env>) -> Result<D1Database, AppError> {
-    env.d1("vault1").map_err(AppError::Worker)
+    env.d1("vaultsql").map_err(AppError::Worker)
 }
